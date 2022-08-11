318bbcdf03ba4a1c2cf26aaa2237e9a3e0d17fd1
Alejandra Toro <torogrisalesa@gmail.com>
2022-08-10 20:35:46 -0500

    Textarea modifica para que se autoajuste al texto. Función para comprobar que hayan mayúsculas terminado

---
+++ script.js
@@ -0,0 +1,31 @@
+var textarea = document.getElementById('write');
+var encrypt = document.getElementById('encrypt');
+var decrypt = document.getElementById('decrypt');
+var noMessage = document.getElementById('no-message');
+var text2 = document.getElementById('text2');
+textarea.addEventListener('keydown',autosize);
+encrypt.onclick = encriptar;
+
+function autosize(){
+    var el = this;
+    setTimeout(function(){
+        el.style.cssText = 'height:auto;padding:0';
+        el.style.cssText = 'height:' + el.scrollHeight+ 'px';
+    },0)
+}
+function comprobarMayuscula(){
+    var textoEncriptar = textarea.value;
+    var comprobacion = false;
+    for(var i=0;i<textoEncriptar.length;i++){
+        if(textoEncriptar.charCodeAt(i)>=65 && textoEncriptar.charCodeAt(i)<=90){
+            comprobacion = true;
+            break;
+        }
+    }
+    return comprobacion;
+}
+function encriptar(){
+    var textoEncriptar = textarea.value;
+    
+}
+
