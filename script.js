501aaf41915f326ef35937af82d7d9855ef6f2b7
Alejandra Toro <torogrisalesa@gmail.com>
2022-08-15 14:35:54 -0500

    Función desencriptar terminada

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ textarea.addEventListener('keydown',autosize);
 encrypt.onclick = encriptar;
 var copy = document.getElementById('copy');
 copy.onclick = copiar;
+decrypt.onclick = desencriptar;
 
 function autosize(){
     var el = this;
@@ -89,3 +90,48 @@ function copiar(){
     var res = document.execCommand('copy');
     window.getSelection().removeRange(seleccion);
 }
+
+function desencriptar(){
+    var mensajeDesencriptado = [];
+    for(var i= 0; i<textarea.value.length;i++){
+        mensajeDesencriptado.push(textarea.value.charAt(i));
+    }
+    for(var i = 0;i<textarea.value.length;i++){
+        if(i<(textarea.value.length-1)){
+            if(mensajeDesencriptado[i]=="a" && mensajeDesencriptado[i+1]=="i"){
+                mensajeDesencriptado.splice(i+1,1);
+            }    
+        }
+        if(i<(textarea.value.length-3)){
+            if((mensajeDesencriptado[i]=="i" && mensajeDesencriptado[i+1]=="m") && (mensajeDesencriptado[i+2]=="e" && mensajeDesencriptado[i+3]=="s")){
+                mensajeDesencriptado.splice(i+1,3);
+            }else if((mensajeDesencriptado[i]=="o" && mensajeDesencriptado[i+1]=="b") && (mensajeDesencriptado[i+2]=="e" && mensajeDesencriptado[i+3]=="r")){
+                mensajeDesencriptado.splice(i+1,3);
+            }else if((mensajeDesencriptado[i]=="u" && mensajeDesencriptado[i+1]=="f") && (mensajeDesencriptado[i+2]=="a" && mensajeDesencriptado[i+3]=="t")){
+                mensajeDesencriptado.splice(i+1,3);
+            }
+        }
+        if(i<(textarea.value.length-4)){
+            if(((mensajeDesencriptado[i]=="e" && mensajeDesencriptado[i+1]=="n") && (mensajeDesencriptado[i+2]=="t" && mensajeDesencriptado[i+3]=="e")) && mensajeDesencriptado[i+4]=="r"){
+                mensajeDesencriptado.splice(i+1,4);
+            }
+        }
+    }
+    mensajeDesencriptado = mensajeDesencriptado.join("");
+    mensajeDesencriptado = mensajeDesencriptado.toString();
+    noMessage.style.fontSize = '20px';
+    noMessage.style.width = '263px';
+    noMessage.style.height ='auto';
+    noMessage.style.wordBreak ='break-all';
+    noMessage.style.width='100%';
+    noMessage.style.textAlign = 'center';
+    noMessage.innerHTML = mensajeDesencriptado;
+    text.style.visibility = 'hidden';
+    text.style.position = 'absolute';
+    copy.style.display = 'block';
+    textarea.value = '';
+    
+}
+    
+
+
